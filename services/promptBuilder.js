636ebf1244066b6b418bc5d3ab8e511931e08ef6
SyedFrazAli <work.frazali@gmail.com>
2025-12-31 08:40:10 +0000

    fix: add missing closing paren to promptBuilder logger import

--- services/promptBuilder.js
+++ services/promptBuilder.js
@@ -1,5 +1,5 @@
 // services/promptBuilder.js
-const logger = require('../utils/logger'
+const logger = require('../utils/logger');
 class PromptBuilder {
     constructor() {  // LinkedIn post constraints
     this.minLength = 200;
